cff8da2381d63a3e6431e966b3b7aaedff083247
larabee7777 <58976112+larabee7777@users.noreply.github.com>
2024-09-12 20:59:20 +0200

    testing ec2 pipeline

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -10,17 +10,6 @@ function App() {
           Congratulations on successfully deploying a React app on your AWS EC2
           instance!
         </h3>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
