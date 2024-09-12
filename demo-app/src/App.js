e7c9d7d725258121fc6a6030b45436a8c96c8b33
larabee7777 <58976112+larabee7777@users.noreply.github.com>
2024-09-12 21:03:47 +0200

    pipeline test 2

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Congratulations on successfully deploying a React app on your AWS EC2
           instance!
         </h3>
+        <p>This is v1.1</p>
       </header>
     </div>
   );
