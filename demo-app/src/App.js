f15c19a17bf687c3a14faf75329a64197dcc6b1a
larabee7777 <58976112+larabee7777@users.noreply.github.com>
2024-09-12 22:21:19 +0200

    v9

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Congratulations on successfully deploying a React app on your AWS EC2
           instance!
         </h3>
-        <p>This is v1.8</p>
+        <p>This is v1.9</p>
       </header>
     </div>
   );
