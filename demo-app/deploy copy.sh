461f77ee540d5742f59eb25b196d52366344422d
larabee7777 <58976112+larabee7777@users.noreply.github.com>
2024-09-13 13:02:58 +0200

    update deploy and yaml files for deploy stage

--- demo-app/deploy copy.sh
+++ demo-app/deploy copy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Stop the running app if it's already running (ignore error if not running)
-pm2 stop react-app || true
-
 # Navigate to the app directory on the EC2 instance
 cd /home/ec2-user/app/react-app-demo/demo-app/
 
 # Ensure dependencies are up-to-date (if needed)
 npm install
+
+# Stop the running app if it's already running (ignore error if not running)
+pm2 stop react-app || true
 
 # Serve the new build with PM2 on port 3000
 pm2 serve build 3000 --name react-app
